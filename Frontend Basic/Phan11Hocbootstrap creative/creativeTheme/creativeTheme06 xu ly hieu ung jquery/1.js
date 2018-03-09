0560fb2e3d399a334973b3a958181a26cfd09f32
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-09 17:30:40 +0700

    hihi

---
+++ Frontend Basic/Phan11Hocbootstrap creative/creativeTheme/creativeTheme06 xu ly hieu ung jquery/1.js
@@ -0,0 +1,19 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+ 		if($('html,body').scrollTop()>400){
+
+ 		$('.logo').addClass('maucam')
+ 		$('.navbar').addClass('nentrang')
+ 		.removeClass('navbar-dark')
+ 		.addClass('navbar-light')
+
+ 		}else{
+
+ 		$('.logo').removeClass('maucam')
+ 		$('.navbar').removeClass('nentrang')
+ 		.addClass('navbar-dark')
+ 		.removeClass('navbar-light')
+ 		}
+ 	})
+})  
+ 

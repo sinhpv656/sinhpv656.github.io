d11d43f1a3123c9c03fcdadcb5ed9bdfeb1cf8e4
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-13 04:40:04 +0700

    1

---
+++ Frontend Basic/project velocity code mẫu/09/1.js
@@ -0,0 +1,21 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+
+ 		if($('body').scrollTop()>300) {
+ 			$('.logo').addClass('chucam');
+ 			$('.nutlen').addClass('hienthi')
+
+ 		}
+ 		else if($('body').scrollTop()<=100) {
+			$('.logo').removeClass('chucam');
+			$('.nutlen').removeClass('hienthi')
+ 		}
+ 	})
+
+
+ 	$('.nutlen').click(function(){
+ 		$('body').animate({'scrollTop':0});
+ 		 
+ 	})
+})  
+ 

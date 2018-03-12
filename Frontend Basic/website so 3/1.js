4c5110d03a188acad98bdff2faad384e462a484c
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-12 22:51:29 +0700

    hihi

---
+++ Frontend Basic/website so 3/1.js
@@ -0,0 +1,9 @@
+﻿ $(function(){
+ 	//khi click vao phan tu len tren  -> 
+
+ 	$('.lentren').click(function(){
+ 		$('body').animate({scrollTop:0});
+ 		return false;
+ 	})
+})  
+ 
---
+++ Frontend Basic/website so 5/1.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+	$(window).scroll(function(event) {
+		/* console.log(' Ban vua cuon chuot den vi tri ');
+		 console.log($('body').scrollTop());*/
+
+		 var vitri = $('body,html').scrollTop(); 
+		 if(vitri >= 400) {
+		 	// console.log("Tada ! xu ly giao dien menu di ");
+		 	 $('#exCollapsingNavbar2').addClass('bienhinh');
+		 }
+		 else 
+		 {
+		 	$('#exCollapsingNavbar2').removeClass('bienhinh');
+
+		 }
+	});;
+});

ee0983f0369b251ec10989e0f1c9dfdac7f2e237
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-20 11:19:44 +0700

    1

--- jqueryfedufilemau/16 one -page website/file demo theo video phan cuoi/bai2.js
+++ jqueryfedufilemau/16 one -page website/file demo theo video phan cuoi/bai2.js
@@ -1,8 +1,15 @@
 $(function() {
 	/*console.log('chay chua ha cac ban ');
+
+
+
+		//hàm tính vị trí 1 phần tử
 	console.log('tinh thu vi tri bang ham .offset().top');
 	console.log($('.chapter1').offset().top);
+
 */
+
+// $(''); phím tắt: selector;
 
 	$('.menutren ul li:nth-child(1) a').on('click', function(event) {
 		event.preventDefault(); 
@@ -12,10 +19,9 @@ $(function() {
 
 		// $('body').animate({scrollTop:900},1000);
 		//cuộn xuống vị trí 900 so vs top , 1000: tốc độ 1s, 400: tốc độ 0,4s
+		// thủ công vcc
 
 		$('body,html').animate({	scrollTop: $('.chapter1').offset().top},1400,"easeOutCubic");
-		//
-
 
 
 
@@ -24,14 +30,15 @@ $(function() {
 		event.preventDefault();
 		/* Act on the event */
 
-		$('body,html').animate({	scrollTop: $('.chapter2').offset().top},1400,"easeOutCubic");
+		$('body,html').animate({	scrollTop: $('.chapter2').offset().top},1400,"easeOutBounce");
 	});
 
 $('.menutren ul li:nth-child(3) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 
-		$('body,html').animate({	scrollTop: $('.chapter3').offset().top},1400,"easeOutCubic");
+		$('body,html').animate({	scrollTop: $('.chapter3').offset().top},1400,"easeOutExpo");
+		//giống như ô tô đến chỗ đỗ phanh từ từ
 	});
 
 
@@ -40,7 +47,7 @@ $('.menutren ul li:nth-child(4) a').on('click', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 
-		$('body,html').animate({	scrollTop: $('.chapter4').offset().top},1400,"easeOutCubic");
+		$('body,html').animate({	scrollTop: $('.chapter4').offset().top},1400,"easeInOutBack");
 	});
 
 
@@ -52,11 +59,10 @@ $('.nutlentren').on('click', function(event) {
 		$('body,html').animate({	scrollTop: 0},400,"easeInOutElastic");
 	});
 
-$('.menutren ul li:nth-child(4) a').on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
+// $('.menutren ul li:nth-child(4) a').on('click', function(event) {
+// 		event.preventDefault();
 
-		$('body,html').animate({	scrollTop: $('.chapter4').offset().top},1400,"easeOutCubic");
-	});
+// 		$('body,html').animate({	scrollTop: $('.chapter4').offset().top},1400,"easeOutCubic");
+// 	});
 
 });
--- jqueryfedufilemau/24 bai 3 ap dung kien thuc bai 1 bai2/dinh kem 7-7 tong ket/js/bai3.js
+++ jqueryfedufilemau/24 bai 3 ap dung kien thuc bai 1 bai2/dinh kem 7-7 tong ket/js/bai3.js
@@ -7,16 +7,15 @@ $(function() {
 		$(this).next().slideToggle(900,"easeInOutExpo");
 
 		// su dung ham animate de chuyen noidung len 
-		$('body').animate({scrollTop: $(this).offset().top},900,"easeInOutExpo")
+		$('body,html').animate({scrollTop: $(this).offset().top},900,"easeInOutExpo")
 	});
 
 	// code cho phan phong to anh 
-	$('.vaicaianh a').fancybox(); 
+	$('.vaicaianh a').fancybox({openEffect:'elastic'}); 
+	//{openEffect:'elastic'} có or ko : thêm hiệu ứng co dãn ảnh khi nhấn vào ảnh
 
 
 	// xư lý co dan nội dung accordion
 	$('.ndkhoi').slideUp();
-
-	
 
 });

d4a723727a703f5e81925b68fa2c4a757b82e7b0
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-20 14:47:36 +0700

    1

---
+++ jqueryfedufilemau/44 demo 6vidu phan 8-phan 9/baidemo2  video 029 co tweenmax/js/1.js
@@ -0,0 +1,7 @@
+$(function() {
+ 	TweenMax.staggerFrom($('.tinto'),0.5,{top:100,opacity:0},0.1)
+ 	// TweenMax.from($('.tinto'),0.5,{top:100,opacity:0})
+ 	// xử lý khối tin to, thời gian chuyển động 0,5s đi từ dưới trở lên 1op 100px, opacity =0,
+ 	//staggerFrom chuyển động từ từ mỗi cái cách nhau 0.1s 
+
+ });
--- jqueryfedufilemau/44 demo 6vidu/baidemo2 co tweenmax/js/1.js
+++
@@ -1,3 +0,0 @@
-$(function() {
- 	TweenMax.staggerFrom($('.tinto'),0.5,{top:100,opacity:0},0.1)
- });

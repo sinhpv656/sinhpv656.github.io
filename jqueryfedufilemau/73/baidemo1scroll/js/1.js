55e044835545c74eb4c9f90afc35df954e0abbe9
sinhpv656 <36873392+sinhpv656@users.noreply.github.com>
2018-03-21 17:33:58 +0700

    1

--- jqueryfedufilemau/73/baidemo1scroll/js/1.js
+++ jqueryfedufilemau/73/baidemo1scroll/js/1.js
@@ -3,7 +3,7 @@ $(function() {
 		/* console.log(' Ban vua cuon chuot den vi tri ');
 		 console.log($('body').scrollTop());*/
 
-		 var vitri = $('body').scrollTop(); 
+		 var vitri = $('body,html').scrollTop(); 
 		 if(vitri >= 400) {
 		 	// console.log("Tada ! xu ly giao dien menu di ");
 		 	 $('.menutren').addClass('bienhinh');
